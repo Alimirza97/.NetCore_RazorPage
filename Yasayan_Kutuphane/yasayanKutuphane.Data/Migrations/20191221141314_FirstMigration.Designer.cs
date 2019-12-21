902ec33cfe95f10a6441ceafd1e68f55f2bf6740
ZeynepAldinc <56272838+ZeynepAldinc@users.noreply.github.com>
2019-12-21 17:59:12 +0300

    add migration (devamı)

--- Yasayan_Kutuphane/yasayanKutuphane.Data/Migrations/20191221141314_FirstMigration.Designer.cs
+++ Yasayan_Kutuphane/yasayanKutuphane.Data/Migrations/20191221141314_FirstMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Yasayan_Kutuphane.Data;
 
 namespace Yasayan_Kutuphane.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20191221141314_FirstMigration")]
+    partial class FirstMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
